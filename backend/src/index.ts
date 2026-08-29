984b55bdcb1b13970de38d61a224bd1decdc29dd
paweenwat08 <ndaresarth@gmail.com>
2026-08-29 11:42:23 +0700

    change port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ const startServer = async () => {
     try {
         await connectDB();
 
-        const PORT = process.env.PORT || 8000
+        const PORT = process.env.PORT || 4000
 
         const server = app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
